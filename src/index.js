fded7100be9a78654aa25392576955f823dd4d17
barit <mmyers.com@Gmail.com>
2023-08-23 17:45:49 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const createBoongusForm = document.getElementById("createBoongusForm");
+  const listContainer = document.getElementById("boongus")
+  
+  let x = 1;
+  
+  function renderBoongus(boongus) {
+    const li = document.createElement("li");
+    li.className = ("List"+ x);
+    
+    x++;
+
+    const h3 = document.createElement("h3");
+    h3.textContent = boongus.description;
+    li.append(h3.textContent);
+    listContainer.append(li);
+
+  }
+  
+  createBoongusForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newBoongus = {
+      description: e.target.newBoongusDescription.value
+    };
+
+    renderBoongus(newBoongus);
+    createBoongusForm.reset();
+
+  });
+
+})
+
+
+
